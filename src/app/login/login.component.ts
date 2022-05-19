7329e7b413dd14e4aa685b0045acaf5aebc9f1df
rchbinou <SPh9.BOr453E>
2022-05-19 23:26:26 +0100

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { routerTransition } from '../router.animations';
+import {AuthService} from '../shared/services/auth.service';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    animations: [routerTransition()]
+})
+export class LoginComponent implements OnInit {
+    constructor(public router: Router, private authService: AuthService) {}
+
+    ngOnInit() {}
+
+    onLoggedin() {
+        localStorage.setItem('isLoggedin', 'true');
+    }
+}
